1cfed342863c1cb4f4b3b3e51dfde9867d3f5465
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2023-12-29 01:32:06 +0600

    format refactored & tailwind and svelte added for css utilites and ui framework

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
-import keystatic from '@keystatic/astro'
+import keystatic from '@keystatic/astro';
+import svelte from '@astrojs/svelte';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(),keystatic()],
-  output:'hybrid'
+  integrations: [react(), markdoc(), keystatic(), tailwind(),svelte()],
+  output: 'hybrid'
 });
