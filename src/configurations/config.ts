2b8ccb9235d8200465f1a346cc3a1de5ac942f98
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2024-01-03 01:13:52 +0600

    config initiated :tada:

---
+++ src/configurations/config.ts
@@ -0,0 +1,45 @@
+module.exports = {
+  email: "anirdms@gmail.com",
+
+  socialMedia: [
+    {
+      name: "GitHub",
+      url: "",
+    },
+    {
+      name: "Twitter",
+      url: "",
+    },
+    {
+      name: "Linkedin",
+      url: "",
+    },
+    {
+      name: "Codepen",
+      url: "",
+    },
+    {
+      name: "Leetcode",
+      url: "",
+    },
+  ],
+
+  navLinks: [
+    {
+      name: "About",
+      url: "/#about",
+    },
+    {
+      name: "Experience",
+      url: "/#jobs",
+    },
+    {
+      name: "Work",
+      url: "/#projects",
+    },
+    {
+      name: "Contact",
+      url: "/#contact",
+    },
+  ],
+};
