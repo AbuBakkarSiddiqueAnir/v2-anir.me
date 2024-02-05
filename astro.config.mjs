a09c2c73979c62b584f7a225f9bf33caf30f38f9
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2024-02-05 15:59:42 +0600

    git commit -m 'integration (failed ) :... node server integration failed for server  '

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,12 @@ import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-
+import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    markdoc(),
-    keystatic(),
-    tailwind(),
-    svelte(),
-    vercel(),
-  ],
+  integrations: [react(), markdoc(), keystatic(), tailwind(), svelte()],
   output: "hybrid",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
