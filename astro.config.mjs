e8390380f2aa0ebbc36d4029f0e40e8b432f0ba9
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2024-02-07 02:56:14 +0600

    git commit -m 'hero  (refactor ) :...static texts moved to md files'

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,49 @@ import keystatic from "@keystatic/astro";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
-// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic(), tailwind(), svelte()],
+  integrations: [
+    react(),
+    markdoc(),
+    keystatic(),
+    tailwind(),
+    svelte(),
+    mdx({
+      // `syntaxHighlight` inherited from Markdown
+
+      // Markdown `remarkPlugins` ignored,
+      // only `remarkPlugin2` applied.
+      remarkPlugins: [remarkPlugin2],
+      // `gfm` overridden to `false`
+      gfm: false,
+    }),
+  ],
   output: "hybrid",
   adapter: node({
     mode: "standalone",
   }),
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "dracula",
+      // Alternatively, provide multiple themes
+      // https://shikiji.netlify.app/guide/dual-themes#light-dark-dual-themes
+      experimentalThemes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+      // Add custom transformers: https://shikiji.netlify.app/guide/transformers
+      // Find common transformers: https://shikiji.netlify.app/packages/transformers
+      transformers: [],
+    },
+  },
 });
