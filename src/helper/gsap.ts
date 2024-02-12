2c6e9af5a71a2e986fb259c7de20701bb3d5c884
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2024-02-12 16:24:26 +0600

    git commit -m 'hero  (refactor ) : functions export from another file

---
+++ src/helper/gsap.ts
@@ -0,0 +1,33 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export const fadeInAndSlideIn = (
+  element: string | HTMLElement,
+  y = 0,
+  duration = 0.5,
+  ease = "ease"
+) => {
+  return gsap.from(element, {
+    opacity: 0,
+    y,
+
+    duration: duration,
+    ease,
+  });
+};
+
+export const commonElementTriggerSettings = {
+  start: "top 80%",
+  end: "bottom 70%",
+};
+
+export const createTimeline = (trigger: string | null) => {
+  return gsap.timeline({
+    scrollTrigger: {
+      trigger: trigger,
+      // scrub: 1,
+      ...commonElementTriggerSettings,
+    },
+  });
+};
